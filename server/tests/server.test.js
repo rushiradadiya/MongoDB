a3cd2306aef8cd5c293fcc97000c1829d9da5c3b
lcom131-two <radadiyarushita98@gmail.com>
2018-12-14 15:22:10 +0530

    Update with TestCases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,7 +2,7 @@ const expect = require('expect');
 const request = require('supertest');
 const {app} = require('./../server');
 const {Lanet} = require('./../models/lanet');
-
+console.log('hello');
 beforeEach((done)=>{
     Lanet.remove({}).then(()=>done());
 });
